2c39ddcb0fa95adf4d9fdb89d806d0ce424b5c25
현준용 <hjy4649@cocen.com>
2022-10-09 15:22:45 +0900

    mock Server test

--- blog-practice/src/App.vue
+++ blog-practice/src/App.vue
@@ -6,9 +6,19 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
   components: {},
+  created() {
+    axios
+      .get(
+        'https://187757af-e74b-4c0a-9644-ad603fd8c17c.mock.pstmn.io/productList'
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  },
 };
 </script>
 
