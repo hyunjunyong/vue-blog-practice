6e6531d6489fa0a48f481ec0b119069eb859d594
현준용 <hjy4649@cocen.com>
2022-10-09 16:16:29 +0900

    mock data 출력 완료

--- blog-practice/src/App.vue
+++ blog-practice/src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
     <router-link to="/router1">v-ifshow로이동</router-link>
+    <test></test>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import test from './views/test';
 export default {
   name: 'App',
-  components: {},
-  created() {
-    axios
-      .get(
-        'https://187757af-e74b-4c0a-9644-ad603fd8c17c.mock.pstmn.io/productList'
-      )
-      .then((res) => {
-        console.log(res);
-      });
+  components: {
+    test,
   },
 };
 </script>
---
+++ blog-practice/src/views/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="name">
+    <div v-for="(product, index) in productList" :key="index">
+      <p>{{ index }}</p>
+      <p>카태고리 : {{ product.category }}</p>
+      <p>제품가격 : {{ product.price }}</p>
+      <p>제품명 : {{ product.product_name }}</p>
+      <p>배송료 : {{ product.delivery_price }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'name',
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  created() {
+    axios
+      .get(
+        'https://37603f60-d4c5-4142-9505-179ee2fe5cb2.mock.pstmn.io/mockData'
+      )
+      .then((res) => {
+        this.productList = res.data;
+        console.log(this.productList);
+      });
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
