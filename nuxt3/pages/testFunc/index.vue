7d94c47d8abf400bc876973f843538ae08a8c1c9
현준용 <hjy4649@cocen.com>
2023-05-11 09:59:54 +0900

    provide / inject

---
+++ nuxt3/pages/testFunc/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="name"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello!',
+    };
+  },
+  provide() {
+    return {
+      message: this.message,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ nuxt3/pages/testFunc/testInject/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test_inject">{{ message }}</div>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+
+const message = inject('message', 'hello');
+</script>
+
+<style scoped>
+.test_inject {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  height: 300px;
+  border-radius: 10px;
+  width: 20%;
+  margin: 10% 0 0 40%;
+  background-color: yellow;
+  border: 1px solid #ddd;
+}
+</style>
