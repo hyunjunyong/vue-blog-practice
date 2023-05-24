c6c0ae4a9b7714d5af39cf8370f694292f9fa224
현준용 <hjy4649@cocen.com>
2023-05-24 15:38:20 +0900

    vue3 v-model

---
+++ nuxt3/pages/testFunc/testModel/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+import TestInput from './testInput.vue';
+
+const message = ref('hello');
+</script>
+
+<template><TestInput v-model="message" /> {{ message }}</template>
+<style lang="scss" scoped></style>
