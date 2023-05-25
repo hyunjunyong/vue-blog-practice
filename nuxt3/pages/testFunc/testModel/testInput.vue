b013e04dfc21795f4ea086004500968503100582
현준용 <hjy4649@cocen.com>
2023-05-25 15:12:31 +0900

    v-model 다른형태

--- nuxt3/pages/testFunc/testModel/testInput.vue
+++ nuxt3/pages/testFunc/testModel/testInput.vue
@@ -1,11 +1,24 @@
 <script setup>
-defineProps(['modelValue']);
-defineEmits(['update:modelValue']);
+// defineProps(['modelValue']);
+// defineEmits(['update:modelValue']);
+
+const props = defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
 </script>
 
 <template>
-  <input
+  <!-- <input
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
-  />
+  /> -->
+  <input v-model="value" />
 </template>
